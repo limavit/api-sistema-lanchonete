44b9ce4633f2bfce2798f11dccbe2cba972a33b4
limavit <limavit@gmail.com>
2023-08-22 15:19:47 -0300

    Inicial

---
+++ src/main/java/br/com/techlima/ApiSistemaLanchoneteApplication.java
@@ -0,0 +1,13 @@
+package br.com.techlima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSistemaLanchoneteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSistemaLanchoneteApplication.class, args);
+	}
+
+}
